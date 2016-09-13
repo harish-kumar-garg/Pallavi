4b707ef188f961e18ba4c83d65aee7d807d49198
pallaviks <pallaviks.bhat@gmail.com>
2016-09-13 14:04:50 +0530

    Update mongomysql.sh

--- mongomysql.sh
+++ mongomysql.sh
@@ -1,17 +1,3 @@
-﻿sudo su
-cd
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-apt-get update
-apt-get install -y mongodb-org=3.2.8 mongodb-org-server=3.2.8 mongodb-org-shell=3.2.8 mongodb-org-mongos=3.2.8 mongodb-org-tools=3.2.8
-echo "mongodb-org hold" | sudo dpkg --set-selections
-echo "mongodb-org-server hold" | sudo dpkg --set-selections
-echo "mongodb-org-shell hold" | sudo dpkg --set-selections
-echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
-echo "mongodb-org-tools hold" | sudo dpkg --set-selections
-service mongod start
-
-
 mysqlPassword="Welcome123"
 sudo apt-get update
 #no password prompt while installing mysql server
