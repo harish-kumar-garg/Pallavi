800f445a862e1dce376d34b5fefd7305842271e0
pallaviks <pallaviks.bhat@gmail.com>
2016-09-14 14:58:33 +0530

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -20,3 +20,16 @@ sudo apt-get -y install mysql-server-5.6
 #without -p here means the initial password is empty
 
 #sudo service mysql restart
+
+
+cd
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+apt-get update
+apt-get install -y mongodb-org=3.2.8 mongodb-org-server=3.2.8 mongodb-org-shell=3.2.8 mongodb-org-mongos=3.2.8 mongodb-org-tools=3.2.8
+echo "mongodb-org hold" | sudo dpkg --set-selections
+echo "mongodb-org-server hold" | sudo dpkg --set-selections
+echo "mongodb-org-shell hold" | sudo dpkg --set-selections
+echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+service mongod start
