64180023d7640de81b0878e112c77f7c59c8c476
pallaviks <pallaviks.bhat@gmail.com>
2016-09-14 15:04:47 +0530

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -22,6 +22,7 @@ sudo apt-get -y install mysql-server-5.6
 #sudo service mysql restart
 
 
+sudo su
 cd
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
