5cd7c498ff1a726f064adf085411f274149ae5a3
pallaviks <pallaviks.bhat@gmail.com>
2016-09-26 14:04:52 +0530

    Update mysqlforsonar.sh

--- mysqlforsonar.sh
+++ mysqlforsonar.sh
@@ -4,8 +4,8 @@ apt-get update
 # set up a silent install of MySQL
 
 export DEBIAN_FRONTEND=noninteractive
-echo mysql-server-5.6 mysql-server/root_password password welcome123 | debconf-set-selections
-echo mysql-server-5.6 mysql-server/root_password_again password welcome123 | debconf-set-selections
+echo mysql-server-6.0 mysql-server/root_password password Welcome123 | debconf-set-selections
+echo mysql-server-6.0 mysql-server/root_password_again password Welcome123 | debconf-set-selections
 
 # install the LAMP stack
 apt-get -y install mysql-server 
@@ -13,6 +13,6 @@ apt-get -y install mysql-server
 sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
 
 sudo service mysql restart
-echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'welcome123'; flush privileges;" | mysql -u root -pwelcome123
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'Welcome123'; flush privileges;" | mysql -u root -p Welcome123
 
-echo "CREATE DATABASE sonar CHARACTER SET utf8 COLLATE utf8_general_ci; CREATE USER 'sonar' IDENTIFIED BY 'sonar';GRANT ALL PRIVILEGES ON sonar.* TO 'sonar'@'%' IDENTIFIED BY 'sonar'; GRANT ALL ON sonar.* TO 'sonar'@'localhost' IDENTIFIED BY 'sonar'; flush privileges;" | mysql -u root -pwelcome123
+echo "CREATE DATABASE sonar CHARACTER SET utf8 COLLATE utf8_general_ci; CREATE USER 'sonar' IDENTIFIED BY 'sonar';GRANT ALL PRIVILEGES ON sonar.* TO 'sonar'@'%' IDENTIFIED BY 'sonar'; GRANT ALL ON sonar.* TO 'sonar'@'localhost' IDENTIFIED BY 'sonar'; flush privileges;" | mysql -u root -p Welcome123
